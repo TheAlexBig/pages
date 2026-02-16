bb0bab9ea77350a4fb3859301295f2be290c8794
José Valladares <32801195+TheAlexBig@users.noreply.github.com>
2026-02-15 23:40:24 -0600

    Add SITE_TITLE and SITE_DESCRIPTION constants

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,4 @@
 export const SITE_TITLE = 'José Valladares';
 export const SITE_DESCRIPTION =
   'DevOps Engineer | Platform Automation | Cloud & Security. Puppet, GitHub Actions, Zabbix, Terraform, AWS, GCP.';
+
